067774f7d1e9632b014a88ee7d0563bf651691f9
sumannaali <samanaabbas1996@gmail.com>
2025-07-14 03:25:57 -0700

    summary

--- examportal_project/exam_app/views.py
+++ examportal_project/exam_app/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-#def home(request):
-    #return render(request,'exam\login.html')
-
-#def home(request):
-    #return render(request,'exam/register.html')
+def register_view(request):
+    form=RegisterForm()
+    if request.method=='POST':
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Registration succesful")
+            return redirect('exam')
+    return render(request,'exam_app'/register.html',{'form':form})
 
 def home(request):
-    return render(request,'exam/dashboard.html')  
+    return render(request,'exam/register.html')
+
+# def home(request):
+    # return render(request,'exam/dashboard.html')  
+
+# def home(request):
+    # return render(request,'exam/otp.html') 
